c83e4ea1c8ca6f2c11465ff0eb38b8cd62ff39c1
jhanvi857 <jhanvip8507@gmail.com>
2026-08-23 16:05:31 +0530

    test(fixtures): add S6-S10 tiered fixtures and golden-file assertion test suite

---
+++ tests/testdata/S10_multi_author/handler.go
@@ -0,0 +1,11 @@
+package api
+
+<<<<<<< HEAD
+func HandleRequest(r *Request) Response {
+	return handleAuthRequest(r)
+}
+=======
+func HandleRequest(r *Request) Response {
+	return handleOAuthRequest(r)
+}
+>>>>>>> branch
